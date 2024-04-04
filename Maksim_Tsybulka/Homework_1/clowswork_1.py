65b664b4bb68463a5ab723556405e51e2045f482
Maksim Tsybulko <mtsybulko@ozon.ru>
2024-04-04 09:52:45 +0300

    new files

--- Maksim_Tsybulka/Homework_1/clowswork_1.py
+++ Maksim_Tsybulka/Homework_1/clowswork_1.py
@@ -1,3 +1,7 @@
-"""Module providing a function printing python version."""
+class Cat:
+    def meow(self, number_of_meow):
+        print("Meow" * number_of_meow)
+        return number_of_meow
 
-pass
+
+CAT = Cat().meow(42)
