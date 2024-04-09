921c936e4cf721f45126d375e636b8d5045c0164
S-Borozna <ya.svetlana17@gmail.com>
2024-04-09 22:02:44 +0300

    HomeWork_3 done

---
+++ Svetlana_Borozna/HomeWork_3/HomeWork_3.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+# 1. Пример с использованием списка параметров
+@pytest.mark.parametrize("first_name, second_name", [(1, 2), ('max', 'test'), ('testing', 2), (3, 'hi')])
+def test_example(first_name, second_name):
+    print(first_name)
+    print(second_name)
