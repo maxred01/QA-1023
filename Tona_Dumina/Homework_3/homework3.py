ae0d78972c6102420e5dfb38e62ff629ac1c22fe
tonadumina <tonadumina17@gmail.com>
2024-04-13 20:01:43 +0300

    Fixed a bug in the code

--- Tona_Dumina/Homework_3/homework3.py
+++ Tona_Dumina/Homework_3/homework3.py
@@ -1,10 +1,10 @@
 import pytest
 
-#1.Пример с использованием списка параметров
+# 1. Пример с использованием списка параметров
 
 
-@pytest.mark.parametrize("first_name,second_name", [(1, 2, 'max','test'), ('testing', 3)])
+@pytest.mark.parametrize("first_name, second_name", [(1, 2), ('max', 'test'), ('testing', 3)])
 def test_example(first_name, second_name):
     print(first_name)
     print(second_name)
- # [missing-final-newline]
+# [missing-final-newline]
