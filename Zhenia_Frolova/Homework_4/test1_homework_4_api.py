6b2d2f19755ae4d786abd23e019ff68d59e8a496
Evgesha6 <shirshovazhenia1@gmail.com>
2024-04-14 23:51:10 +0300

    homework_4 done

--- Zhenia_Frolova/Homework_4/test1_homework_4_api.py
+++ Zhenia_Frolova/Homework_4/test1_homework_4_api.py
@@ -3,8 +3,8 @@ import pytest_check as check
 
 
 def test_api_register():
-
-    r = requests.post('https://reqres.in/api/register', json={"email": "eve.holt@reqres.in", "password": "pistol"})
+    r = requests.post('https://reqres.in/api/register', json={"email": "eve.holt@reqres.in",
+                                                              "password": "pistol"})
     status_code = r.status_code
 
     check.equal(status_code, 200, f'Статус код не равен 200. Статус код равен {status_code}')
