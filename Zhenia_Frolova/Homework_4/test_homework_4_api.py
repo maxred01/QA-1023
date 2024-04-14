99450d97405df2909d5d45dfe7933c7409e0d253
Evgesha6 <shirshovazhenia1@gmail.com>
2024-04-14 23:47:35 +0300

    homework_4 done

--- Zhenia_Frolova/Homework_4/test_homework_4_api.py
+++ Zhenia_Frolova/Homework_4/test_homework_4_api.py
@@ -1,8 +1,9 @@
 import requests
 import pytest_check as check
+
+
 def test_api_users_page():
     r = requests.get('https://reqres.in/api/users/23')
     status_code = r.status_code
 
     check.equal(status_code, 404, f'Статус код не равен 200. Статус код равен {status_code}')
-
