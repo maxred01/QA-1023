efa4e8c3e61d71b72941a5a253860058c4825442
Kristina_Pershina <badmovochka@gmail.com>
2024-04-15 21:22:56 +0300

    Classwork 3 - use of requests library

---
+++ Kristina_Pershina/Classwork_3/test_classwork_3.py
@@ -0,0 +1,31 @@
+import requests, pytest, pytest_check as check, allure
+
+# @allure.feature('ID check')
+# def test_api_users_page():
+#     r = requests.get('https://reqres.in/api/users/2')
+#     status_code = r.status_code
+#
+#     with allure.step('step 1'):
+#         check.equal(status_code, 200, f'Status code is not 200. The code is {status_code}')
+#
+#     with allure.step('step 2'):
+#         data = r.json()
+#         check.equal(data['data']['id'], 2, f"ID is NOT 2. ID is {data['data']['id']}")
+
+
+@allure.feature('New user')
+def test_api_users_page():
+    data = {"name": "Vasya",
+            "job": "Senior cleaner"
+            }
+    r = requests.post('https://reqres.in/api/users', data=data)
+    status_code = r.status_code
+
+    with allure.step('step 1'):
+        check.equal(status_code, 201, f'Status code is not 201. The code is {status_code}')
+
+    with allure.step('step 2'):
+        new_user_data = r.json()
+        check.equal(new_user_data['name'], 'Vasya', f'Name is wrong.')
+        check.equal(new_user_data['job'], 'Senior cleaner', f'Job is wrong.')
+    # print(r.json())
