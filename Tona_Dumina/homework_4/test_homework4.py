b7eee8ba6bfdfe1e47eede4949b67c0bf6706da7
tonadumina <tonadumina17@gmail.com>
2024-04-17 15:50:24 +0300

    Get and post request sent

---
+++ Tona_Dumina/homework_4/test_homework4.py
@@ -0,0 +1,10 @@
+import requests
+import pytest_check as check
+
+
+def test_api_user_not_found():
+    r = requests.get('https://reqres.in/api/users/23')
+    status_code = r.status_code
+
+    check.equal(status_code, 404, f'Статус код равен {status_code}'
+
