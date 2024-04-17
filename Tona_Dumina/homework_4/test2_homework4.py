58cd63a38d690c61e61656805b588e7d476ce38d
tonadumina <tonadumina17@gmail.com>
2024-04-17 16:06:57 +0300

    Get and post request sent

--- Tona_Dumina/homework_4/test2_homework4.py
+++ Tona_Dumina/homework_4/test2_homework4.py
@@ -6,4 +6,4 @@ def test_api_register():
     r = requests.post("https://reqres.in/api/register",
                       json={"email": "eve.holt@reqres.in", "password": "pistol"})
 
-    check.equal(r.status_code, 200, f"Статус код{r.status_code}")
+    check.equal(r.status_code, 200, f'Статус код{r.status_code}')
