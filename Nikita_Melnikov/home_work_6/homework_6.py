cb74cf12ab0df70800e4eba39db0f88954c1852e
vokinlematikin <on_exe_on@mail.ru>
2024-04-24 15:49:37 +0300

    homework 6 done

--- Nikita_Melnikov/home_work_6/homework_6.py
+++ Nikita_Melnikov/home_work_6/homework_6.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 driver = webdriver.Chrome()
@@ -9,11 +10,13 @@ driver.set_window_size(1600, 800)
 
 driver.get('https://hoster.by/service/domains/')
 
-input_search_box = driver.find_element(By.XPATH, '//section//*[@name="domain_name"]')
+input_search_box = (driver.find_element
+                    (By.XPATH, '//section//*[@name="domain_name"]'))
 
 input_search_box.send_keys('https://nikitamelnikov.by')
 
-input_search_button = driver.find_element(By.XPATH, '//*[@id="domain"]')
+input_search_button = (driver.find_element
+                       (By.XPATH, '//*[@id="domain"]'))
 
 input_search_button.click()
 
