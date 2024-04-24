1f7803e393e77ea52245b0d0edbcc038e156240e
vokinlematikin <on_exe_on@mail.ru>
2024-04-24 17:15:18 +0300

    Homework_6 done

---
+++ Nikita_Melnikov/Homework_6/test_homework_6.py
@@ -0,0 +1,23 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.set_window_size(1600, 800)
+
+driver.get('https://hoster.by/service/domains/')
+
+input_search_box = (driver.find_element
+                    (By.XPATH, '//section//*[@name="domain_name"]'))
+
+input_search_box.send_keys('nikitamelnikov.by')
+
+input_search_button = (driver.find_element
+                       (By.XPATH, '//*[@id="domain"]'))
+
+input_search_button.click()
+
+time.sleep(10)
+
+driver.close()
