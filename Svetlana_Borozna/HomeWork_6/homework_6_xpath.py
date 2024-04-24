43f4ca405de7c8ae8046a82c5e039308573fdf68
S-Borozna <ya.svetlana17@gmail.com>
2024-04-24 12:15:21 +0300

    HomeWork_6 done ClasseWork_6 исправлены ошибки(исправлено превышение символов в строке)

---
+++ Svetlana_Borozna/HomeWork_6/homework_6_xpath.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+
+driver.get('https://hoster.by/service/domains/')
+
+search_box = driver.find_element(By.XPATH, '//*[@name="domain_name"]')
+
+search_button = driver.find_element(By.XPATH, '//*[@id="domain"]')
+
+search_box.send_keys("Penguins.by")
+
+search_button.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+driver.close()
