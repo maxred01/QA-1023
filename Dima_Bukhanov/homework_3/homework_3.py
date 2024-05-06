5bde97f7e4112dc8306c254fedbf10b302666e07
DimON13124 <dimabukhanoff2705@mail.ru>
2024-05-06 23:20:59 +0300

    homework_2 done

--- Dima_Bukhanov/homework_3/homework_3.py
+++ Dima_Bukhanov/homework_3/homework_3.py
@@ -1,8 +1,9 @@
 import pytest
 
-
 # 1. Пример с использованием списка параметров
+
+
 @pytest.mark.parametrize("first_name, second_name", [(1, 2, 'max', 'test'), ('testing', 2, 3)])
 def test_example(first_name, second_name):
-    print(first_name)
+    print(first_name),
     print(second_name)
